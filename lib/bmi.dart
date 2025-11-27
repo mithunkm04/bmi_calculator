2b15ce8fe831d91362a4736cd0fc20a90367d9ee
mithunkm04 <mithunkrishnakm04@gmail.com>
2025-11-27 09:42:06 +0530

    completed

--- lib/bmi.dart
+++ lib/bmi.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
 
-class Bmi extends StatelessWidget {
+class Bmi extends StatefulWidget {
   const Bmi({super.key});
 
+  @override
+  State<Bmi> createState() => _BmiState();
+}
+
+class _BmiState extends State<Bmi> {
+  TextEditingController heightcon = TextEditingController();
+  TextEditingController weightcon = TextEditingController();
+  String result="";
+  void calbmi(){
+    double height = double.parse(heightcon.text)/100;
+    double weight = double.parse(weightcon.text);
+    double bmi = weight/(height*height);
+    setState(() {
+      result="your bmi is ${bmi.toStringAsFixed(2)}";
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,48 +46,59 @@ class Bmi extends StatelessWidget {
                 width: 230,
                 child: Card(
                   color: Colors.white,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      TextField(
-                        decoration: InputDecoration(
-                          prefixIcon: Icon(Icons.height),
-                          hintText: "Height(cm)",
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(4),
-                          ),
-                          focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(4))
-                        ),
-                      ),
-                      TextField(
-                        decoration: InputDecoration(
-                          prefixIcon: Icon(Icons.line_weight),
-                          hintText: "weight(kg)",
-                          enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(4)),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(4),
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        TextField(
+                          controller: heightcon,
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.height),
+                            hintText: "Height(cm)",
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
                           ),
                         ),
-                      ),
-                      Container(
-                        height: 30,
-                        width:double.infinity,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(4),
-                          gradient: LinearGradient(
-                            begin: Alignment.bottomCenter,
-                            end: AlignmentGeometry.topCenter,
-                            colors: [Colors.blue, Colors.black],
+                        TextField(
+                          controller: weightcon,
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.line_weight),
+                            hintText: "weight(kg)",
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
                           ),
                         ),
-                        child: Center(
-                          child: Text(
-                            "Calculate",
-                            style: TextStyle(color: Colors.white),
+                        GestureDetector(onTap: calbmi,
+                          child: Container(
+                            height: 30,
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(4),
+                              gradient: LinearGradient(
+                                begin: Alignment.bottomCenter,
+                                end: AlignmentGeometry.topCenter,
+                                colors: [Colors.blue, Colors.black],
+                              ),
+                            ),
+                            child: Center(
+                              child: Text(
+                                "Calculate",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
                           ),
-                        ),
-                      ),
-                    ],
+                        ),Text(result)
+                      ],
+                    ),
                   ),
                 ),
               ),
